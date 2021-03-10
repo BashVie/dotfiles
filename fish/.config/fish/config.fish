9eee896d44879b917254b197bb80f3faa45a2966
Bash Vollink <bashvie@amalthea.lan1>
2021-03-10 09:38:24 -0600

    rofi and awesome changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,9 @@ starship init fish | source
 
 set fish_greeting ""
 
-set PATH ~/.emacs.d/bin $PATH
+set PATH ~/.emacs.d/bin ~/.local/bin $PATH
 set EDITOR emacs
+set DENO_INSTALL ~/.deno
+set PATH $DENO_INSTALL/bin $PATH
+
+alias mon2cam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
